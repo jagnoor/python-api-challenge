1c078b86b411e1db3d5a8814e64b8fd0a0a694f7
Jag <Jagnoor@gmail.com>
2021-02-10 20:07:55 -0600

    Api created

---
+++ VacationPy/config.py
@@ -0,0 +1,5 @@
+#openweather API 
+api_key = "6a9c7e6e68ad4cd863cd1a354b29b925"
+
+# Enter your API key Google APi  
+gkey = "AIzaSyBT57g9RWiFlfjSFpugsKhYb647Vu4Up6Y"
--- WeatherPy/config.py
+++ WeatherPy/config.py
@@ -1,1 +1,5 @@
-api_key = "YOUR KEY HERE!"
+#openweather API 
+api_key = "6a9c7e6e68ad4cd863cd1a354b29b925"
+
+# Enter your API key Google APi  
+gkey = "AIzaSyBT57g9RWiFlfjSFpugsKhYb647Vu4Up6Y"
